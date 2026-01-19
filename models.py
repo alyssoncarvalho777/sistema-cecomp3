09762fff75bf75d7547bc4b3bc8e8fc9c7f7a727
alyssoncarvalho777 <86637456+alyssoncarvalho777@users.noreply.github.com>
2026-01-19 10:27:30 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, Float, DateTime, Boolean
 from sqlalchemy.orm import relationship, declarative_base 
 # NOTA: Se o erro persistir mesmo com requirements.txt, use a linha abaixo no lugar da linha acima:
 # from sqlalchemy.ext.declarative import declarative_base
