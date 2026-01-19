82d6c9599806cc292a19dee8228cfe41a8e55dd1
alyssoncarvalho777 <86637456+alyssoncarvalho777@users.noreply.github.com>
2026-01-19 09:00:36 -0400

    Create models.py
    Estrutura do Banco de Dados

---
+++ models.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, Float, DateTime
+from datetime import datetime
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    login = Column(String(50), unique=True, nullable=False)
+    senha = Column(String(50), nullable=False)
+
+class Setor(Base):
+    __tablename__ = 'setores'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+
+class Modalidade(Base):
+    __tablename__ = 'modalidades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    fases = relationship("FaseTemplate", back_populates="modalidade", cascade="all, delete-orphan")
+
+class FaseTemplate(Base):
+    __tablename__ = 'fases_template'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100))
+    ordem = Column(Integer)
+    modalidade_id = Column(Integer, ForeignKey('modalidades.id'))
+    modalidade = relationship("Modalidade", back_populates="fases")
+
+class Processo(Base):
+    __tablename__ = 'processos'
+    id = Column(Integer, primary_key=True)
+    numero_sei = Column(String(50), unique=True)
+    objeto = Column(Text)
+    data_autorizacao = Column(DateTime, default=datetime.now)
+    valor_previsto = Column(Float, default=0.0)
+    modalidade_id = Column(Integer, ForeignKey('modalidades.id'))
+    fase_atual = Column(String(100))
+    setor_origem_id = Column(Integer, ForeignKey('setores.id'))
